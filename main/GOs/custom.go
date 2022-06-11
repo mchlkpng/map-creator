a4a3270d36ef30c940467bd528c2cc5d6126f9ca
mchlkpng <thecoyotegamer623@gmail.com>
2022-06-11 04:49:48 -0500

    made changes

--- main/GOs/custom.go
+++ main/GOs/custom.go
@@ -191,3 +191,23 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/main/atlases/16px.atlas\"\n"
+  "default_animation: \"16px\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
