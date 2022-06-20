ab4518ebf92bd0322be4e7bb28094fbd6a248065
mchlkpng <thecoyotegamer623@gmail.com>
2022-06-20 00:54:11 -0500

    ok so

---
+++ main/GOs/multiplayer/mCustom.go
@@ -0,0 +1,53 @@
+components {
+  id: "mScriptloader"
+  component: "/main/scripts/multiplayer/mScriptloader.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "collisionfactory"
+  type: "factory"
+  data: "prototype: \"/main/GOs/colgameobj.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/main/atlases/16px.atlas\"\n"
+  "default_animation: \"16px\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
